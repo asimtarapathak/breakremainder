43e0c979c901c89884ce1851bb29c64706190899
Asim Tara Pathak <50657538+asimtarapathak@users.noreply.github.com>
2020-07-06 20:58:42 +0530

    Add files via upload

---
+++ breakremainder.py
@@ -0,0 +1,128 @@
+import datetime, time
+import win32com.client as wincl
+
+speak = wincl.Dispatch("SAPI.SpVoice")
+
+
+# print ("Current Time : "+str(datetime.datetime.now().hour) + ":" + str(datetime.datetime.now().minute))
+# print()
+while True:
+    if int(datetime.datetime.now().hour)==1 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==2 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==3 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==4 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==5 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        datetime.time(5,1,00)
+        continue
+
+    if int(datetime.datetime.now().hour)==6 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==7 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==8 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==9 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==10 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==11 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==12 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==13 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==14 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==15 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==16 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==17 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        datetime.time(5,1,00)
+        continue
+
+    if int(datetime.datetime.now().hour)==18 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==19 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==20 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==21 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==22 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==23 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
+
+    if int(datetime.datetime.now().hour)==24 and int(datetime.datetime.now().minute)==00:
+        speak.Speak("Asim, Take a break and have a glass of water")
+        time.sleep(60)
+        continue
